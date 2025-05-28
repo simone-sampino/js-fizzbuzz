5f2adc688106485e97496146cbbb8fd0b5d9d22b
simone-sampino <s.samp98@gmail.com>
2025-05-28 19:18:06 +0200

    Implement FizzBuzz logic with initial number printing

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// stampare numeri da 1 a 100
+for (i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
+// per i multipli di 3 deve stampare “Fizz” al posto del numero
+
+// per i multipli di 5 deve stampare "Buzz" al posto del numero
+
+// per i numeri che sono sia multipli di 3 che di 5 deve stampare FizzBuzz al posto del numero
