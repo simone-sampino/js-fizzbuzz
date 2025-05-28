fbd1d59de7dcb90bb5f36d8bbbf8355b0aaf2680
simone-sampino <s.samp98@gmail.com>
2025-05-28 19:27:04 +0200

    Refactor FizzBuzz logic to correctly handle "FizzBuzz" for multiples of 3 and 5

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 // stampare numeri da 1 a 100
 for (i = 1; i <= 100; i++) {
-    
-    // per i multipli di 3 deve stampare “Fizz” al posto del numero
-    if (i % 3 == 0) {
+  // per i numeri che sono sia multipli di 3 che di 5 deve stampare FizzBuzz al posto del numero
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  }
+  // per i multipli di 3 deve stampare “Fizz” al posto del numero
+  else if (i % 3 == 0) {
     console.log("Fizz");
-  } 
+  }
   // per i multipli di 5 deve stampare "Buzz" al posto del numero
   else if (i % 5 == 0) {
     console.log("Buzz");
@@ -12,6 +15,3 @@ for (i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
-
-
-// per i numeri che sono sia multipli di 3 che di 5 deve stampare FizzBuzz al posto del numero
