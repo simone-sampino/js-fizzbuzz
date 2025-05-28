a653624dc097ff23eea687f78ddd597b930960eb
simone-sampino <s.samp98@gmail.com>
2025-05-28 19:19:28 +0200

    Refactor FizzBuzz logic to correctly print "Fizz" for multiples of 3

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 // stampare numeri da 1 a 100
 for (i = 1; i <= 100; i++) {
+    
+    // per i multipli di 3 deve stampare “Fizz” al posto del numero
+    if (i % 3 == 0) {
+    console.log("Fizz");
+  } else {
     console.log(i);
+  }
 }
 
-// per i multipli di 3 deve stampare “Fizz” al posto del numero
 
 // per i multipli di 5 deve stampare "Buzz" al posto del numero
 
